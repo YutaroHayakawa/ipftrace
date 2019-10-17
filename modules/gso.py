a25f02e1adf0df285eb143d87e796a685e45e78c
YutaroHayakawa <yhayakawa3720@gmail.com>
2019-10-17 08:45:26 +0000

    no-issue: Stringify gso_types

--- modules/gso.py
+++ modules/gso.py
@@ -1,4 +1,34 @@
 from ctypes import *
+from enum import IntFlag
+
+
+class FeatureFlags(IntFlag):
+    SKB_GSO_TCPV4 = 1 << 0
+    SKB_GSO_DODGY = 1 << 1
+    SKB_GSO_TCP_ECN = 1 << 2
+    SKB_GSO_TCP_FIXEDID = 1 << 3
+    SKB_GSO_TCPV6 = 1 << 4
+    SKB_GSO_FCOE = 1 << 5
+    SKB_GSO_GRE = 1 << 6
+    SKB_GSO_GRE_CSUM = 1 << 7
+    SKB_GSO_IPXIP4 = 1 << 8
+    SKB_GSO_IPXIP6 = 1 << 9
+    SKB_GSO_UDP_TUNNEL = 1 << 10
+    SKB_GSO_UDP_TUNNEL_CSUM = 1 << 11
+    SKB_GSO_PARTIAL = 1 << 12
+    SKB_GSO_TUNNEL_REMCSUM = 1 << 13
+    SKB_GSO_SCTP = 1 << 14
+    SKB_GSO_ESP = 1 << 15
+    SKB_GSO_UDP = 1 << 16
+    SKB_GSO_UDP_L4 = 1 << 17
+
+
+def parse_features(features):
+    ret = []
+    for f in FeatureFlags:
+        if features & f:
+            ret.append(str(f.name))
+    return "|".join(ret)
 
 
 def gen_match():
@@ -40,4 +70,4 @@ class GSOData(Structure):
 
 def parse_data(data):
     gd = cast(data, POINTER(GSOData)).contents
-    return f" (gso_size: {gd.gso_size} gso_segs: {gd.gso_segs} gso_type: {gd.gso_type})"
+    return f" (gso_size: {gd.gso_size} gso_segs: {gd.gso_segs} gso_type: {parse_features(gd.gso_type)})"
