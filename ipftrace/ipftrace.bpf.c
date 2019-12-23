f4868f7a21c82437911649333ddfd1f291339704
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2019-12-23 14:21:10 +0900

    no-issue: Separate the headers from source

--- ipftrace/ipftrace.bpf.c
+++ ipftrace/ipftrace.bpf.c
@@ -1,10 +1,3 @@
-#include <linux/skbuff.h>
-#include <uapi/linux/ip.h>
-#include <uapi/linux/tcp.h>
-#include <uapi/linux/udp.h>
-#include <uapi/linux/ipv6.h>
-#include <uapi/linux/ptrace.h>
-
 /*
  * Bowrrowed the idea from tracepkt
  * https://github.com/yadutaf/tracepkt
---
+++ ipftrace/ipftrace.bpf.h
@@ -0,0 +1,6 @@
+#include <linux/skbuff.h>
+#include <uapi/linux/ip.h>
+#include <uapi/linux/tcp.h>
+#include <uapi/linux/udp.h>
+#include <uapi/linux/ipv6.h>
+#include <uapi/linux/ptrace.h>
