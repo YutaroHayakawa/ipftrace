efef8e65a997236e5dab5cd42a4d9685488b2ca2
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2020-01-12 02:59:24 +0900

    no-issue: Cleanup the example modules and manifests

--- ipftrace/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from ipftrace.modules.gso import GSOModule
-
-
-def get_modules():
-    return {
-        "gso": GSOModule
-    }
--- ipftrace/modules/module.py
+++
@@ -1,6 +0,0 @@
-class IPFTraceModule:
-    def gen_match(self):
-        raise NotImplementedError()
-
-    def parse_data(self, data):
-        raise NotImplementedError()
